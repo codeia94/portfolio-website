49a8b1aabbf3833b3bb25d6a1c47489f22e77f1c
Leia <leiaooi.94@gmail.com>
2024-08-14 03:53:23 -0700

    contact page with icons embedded with links

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,11 +1,13 @@
 import HomePage from './home/page';
 import About from './about/page';
+import Contact from './contact/page';
 
 export default function Home() {
   return (
 		<main className='px-6'>
 			<HomePage />
 			<About />
+			<Contact />
 		</main>
   );
 }
