70f221a8c7983bbe30e1763a61d19837a03aebef
Leia <leiaooi.94@gmail.com>
2024-08-15 13:03:59 -0700

    Change the e for resume to é

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
 			<div className='flex justify-center'>
 				<a href='/resume.pdf' download type="button" className='flex items-center justify-center h-9 w-32 rounded-full bg-galaxyblack text-white p-[1.5px] group relative overflow-hidden border-2 border-teal hover:text-galaxyblack'>
 					<div className='absolute inset-0 bg-teal h-full w-full flex items-center justify-center transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-linear origin-left'></div>
-					<span className='relative z-10 text-base '>Resume</span>
+					<span className='relative z-10 text-base '>Résumé</span>
 				</a>
 			</div>
 			<div></div>
