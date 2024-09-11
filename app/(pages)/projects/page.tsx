0f577088cbffc2cead0f7b797c2d0ddfcb6be76d
Leia <leiaooi.94@gmail.com>
2024-09-10 22:04:01 -0700

    style: change flex position for projects when screen:md

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -6,12 +6,14 @@ export default function Project () {
 		<section id='projects' className='min-h-screen flex flex-col justify-center items-center gap-y-4 '>
 			<div className='title'>Projects</div>
 			{projects.map((project, index) => (
-				<article key={index} className='article-card article-card-border'>
-					<img  
-						src={project.project_image} 
-						className='h-56 w-full object-cover'
-					/>
-					<div className='bg-white p-4 sm:p-6'>
+				<article key={index} className='article-card article-card-border article-card-md'>
+					<div className='md:flex-1'>
+						<img  
+							className='h-56 w-full object-cover md:h-full'
+							src={project.project_image} 
+						/>
+					</div>
+					<div className='bg-white p-4 sm:p-6 md:flex-1'>
 						<div className='flex justify-between items-center flex-wrap'>
 							<a href={project.link} target='_blank' className='group' >
 								<h3 className='mt-0.5 text-lg font-medium text-galaxyblack underline hover:text-greenpea flex flex-row'>{project.name}<IconArrowUpRight className='w-4 link-icon'/></h3>
@@ -30,15 +32,16 @@ export default function Project () {
 									</a>
 								)}
 							</div>
-
 						</div>
-						<p className='my-2 line-clamp-4 text-sm/relaxed text-gray-500 overflow-auto'>{project.description}</p>
-						<div className='flex flex-wrap gap-1 '>
-							{project.skills && project.skills.map((skill, skillIndex) => (
-								<div className='techStack-list' key={skillIndex}>
-									<p>{skill}</p>
-								</div>
-							))}
+						<div className='article-desc-tech'>
+							<p className='my-2 line-clamp-4 text-sm/relaxed text-gray-500 overflow-auto article-desc'>{project.description}</p>
+							<div className='flex flex-wrap gap-1 '>
+								{project.skills && project.skills.map((skill, skillIndex) => (
+									<div className='techStack-list' key={skillIndex}>
+										<p>{skill}</p>
+									</div>
+								))}
+							</div>
 						</div>
 					</div>
 				</article>
