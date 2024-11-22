4f9babb75a196733423d7605787d14bae524fecf
Leia <leiaooi.94@gmail.com>
2024-11-22 03:31:56 -0800

    chores: add in new project

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -1,6 +1,14 @@
 export const projects = [
 	{
 		id: 1,
+		name: 'ResuM8',
+		description: "Created for job seekers, by the people. ResuM8 is a platform created by job seekers, for job seekers, helping streamline resumes and track job applications efficiently. Coming soon, features like AI-driven resume optimization, an ATS checker, and personalized templates will enhance the job search experience, all while ensuring secure authentication for your data.",
+		project_image: '/images/projects/resum8.png',
+		skills: ['Javascript', 'Typescript', 'Tailwind'],
+		link: 'https://resum8.vercel.app/',
+	},
+	{
+		id: 1,
 		name: 'SnowCast',
 		description: "SnowCast is a comprehensive platform designed to streamline the experience for snowboarders and skiers by providing all the essential information they need in one place. From detailed hourly and 5-day weather forecasts to downloadable trail maps and live cam feeds, SnowCast ensures that you're fully prepared before hitting the slopes. With its responsive design, SnowCast offers a seamless experience across all devices, making it easier than ever to plan your next winter adventure, whether you're at home or on the go.",
 		project_image: '/images/projects/snowmountain.png',
